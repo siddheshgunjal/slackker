0a06c7a47680fc1ad5e5f225de44994fa8b27665
Siddhesh Gunjal <siddhu19@live.com>
2026-05-22 16:06:53 +0530

    Export DiscordClient from core package

--- slackker/core/__init__.py
+++ slackker/core/__init__.py
@@ -1,6 +1,13 @@
 from slackker.core.client import BaseClient
+from slackker.core.discord import DiscordClient
 from slackker.core.slack import SlackClient
 from slackker.core.teams import TeamsClient
 from slackker.core.telegram import TelegramClient
 
-__all__ = ["BaseClient", "SlackClient", "TelegramClient", "TeamsClient"]
+__all__ = [
+    "BaseClient",
+    "DiscordClient",
+    "SlackClient",
+    "TelegramClient",
+    "TeamsClient",
+]
