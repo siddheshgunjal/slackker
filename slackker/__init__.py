28d603eaa5e5a92910b47f0a5de6fa0b1c525d8b
Siddhesh Gunjal <siddhu19@live.com>
2026-05-26 13:55:50 +0530

    Bump version to 1.5.0

--- slackker/__init__.py
+++ slackker/__init__.py
@@ -11,7 +11,7 @@ from slackker.listener import CommandHandler, MessagePoller
 
 __author__ = "Siddhesh Gunjal"
 __email__ = "siddhu19@live.com"
-__version__ = "1.4.9"
+__version__ = "1.5.0"
 
 # module level doc-string
 __doc__ = """
