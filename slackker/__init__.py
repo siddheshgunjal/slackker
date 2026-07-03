861aa30a5f411831c2a668212041545af73a2f31
Siddhesh Gunjal <siddhu19@live.com>
2026-07-03 17:01:49 +0530

    Bump version to 2.0.2

--- slackker/__init__.py
+++ slackker/__init__.py
@@ -13,7 +13,7 @@ from slackker.mcp.handler import MCPHandler
 
 __author__ = "Siddhesh Gunjal"
 __email__ = "siddhu19@live.com"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 # module level doc-string
 __doc__ = """
